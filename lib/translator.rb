ed3811dabde72bbf29f21d4d21628786aaa69665
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-13 23:46:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ emoticons = YAML.load_file(file_path)
 new_hash = {}
 emoticons.each do |key, value|
   new_hash[key] = {}
-new_hash["get_emoticon"][value[0]] = emoticons[key][1]
-new_hash["get_meaning"][value[1]] = key
+new_hash[key][:english] = value[0]
+new_hash[key][:japanese] = value[1]
 end
 new_hash
 end
